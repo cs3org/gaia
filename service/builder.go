ecd78f49e7bb19f8738abbeff95a265fa5e8cc2a
Gianmaria Del Monte <g.macmount@gmail.com>
2023-07-21 10:55:10 +0200

    add gaia http service

---
+++ service/builder.go
@@ -0,0 +1,241 @@
+// Copyright 2018-2023 CERN
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// In applying this license, CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/cs3org/gaia/pkg/builder"
+	"github.com/cs3org/reva"
+	"github.com/cs3org/reva/pkg/appctx"
+	"github.com/cs3org/reva/pkg/rhttp/global"
+	"github.com/cs3org/reva/pkg/utils/cfg"
+	"github.com/go-chi/chi/v5"
+)
+
+func init() {
+	reva.RegisterPlugin(Builder{})
+}
+
+type Builder struct {
+	router *chi.Mux
+	c      *config
+}
+
+type config struct {
+	BuildFolder      string        `mapstructure:"build_folder"`
+	BinaryTempFolder string        `mapstructure:"binary_temp_folder"`
+	BuildTimeout     time.Duration `mapstructure:"build_timeout"`
+}
+
+func (c *config) ApplyDefaults() {
+	if c.BinaryTempFolder == "" {
+		c.BinaryTempFolder, _ = os.MkdirTemp("", "gaia-*")
+	}
+
+	if c.BuildTimeout == 0 {
+		c.BuildTimeout = 120 * time.Second
+	}
+}
+
+func (Builder) RevaPlugin() reva.PluginInfo {
+	return reva.PluginInfo{
+		ID:  "http.services.gaia",
+		New: New,
+	}
+}
+
+func New(ctx context.Context, m map[string]any) (global.Service, error) {
+	var c config
+	if err := cfg.Decode(m, &c); err != nil {
+		return nil, err
+	}
+	b := Builder{
+		router: chi.NewRouter(),
+		c:      &c,
+	}
+	b.initRouter()
+	return &b, nil
+}
+
+func (s *Builder) initRouter() {
+	s.router.Get("/download", s.download)
+	s.router.Get("/plugins", s.listPlugins)
+	s.router.Post("/plugins", s.registerPlugin)
+}
+
+type downloadRequest struct {
+	OS          string
+	Arch        string
+	RevaVersion string
+	Plugins     []string
+}
+
+func (s *Builder) download(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	log := appctx.GetLogger(ctx)
+
+	req, err := parseDownloadRequest(r)
+	if err != nil {
+		log.Info().Err(err).Msg("error parsing download request")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	plugins, err := parsePlugins(req.Plugins)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	log.Info().
+		Str("os", req.OS).
+		Str("arch", req.Arch).
+		Str("reva_version", req.RevaVersion).
+		Interface("plugins", plugins).
+		Msg("request build of reva")
+
+	b := builder.Builder{
+		Platform: builder.Platform{
+			OS:   req.OS,
+			Arch: req.Arch,
+		},
+		RevaVersion: req.RevaVersion,
+		Log:         log,
+		Plugins:     plugins,
+		TempFolder:  s.c.BuildFolder,
+	}
+
+	output, err := os.CreateTemp(s.c.BinaryTempFolder, "revad")
+	if err != nil {
+		log.Error().Err(err).Msg("error creating temp file for revad")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer output.Close()
+	defer os.RemoveAll(output.Name())
+
+	buildCtx, cancel := context.WithTimeout(ctx, s.c.BuildTimeout)
+	defer cancel()
+	if err := b.Build(buildCtx, output.Name()); err != nil {
+		log.Error().Err(err).Msg("error building reva")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	sendBinary(ctx, w, binaryRevadName(req.OS, req.Arch, req.RevaVersion), output.Name())
+}
+
+func sendBinary(ctx context.Context, w http.ResponseWriter, name, binary string) {
+	log := appctx.GetLogger(ctx)
+
+	file, err := os.Open(binary)
+	if err != nil {
+		log.Error().Err(err).Msgf("error opening file %s", file.Name())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		log.Error().Err(err).Msgf("error statting file %s", file.Name())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Length", strconv.FormatInt(stat.Size(), 10))
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", name))
+	w.Header().Set("Content-Transfer-Encoding", "binary")
+	if _, err := io.Copy(w, file); err != nil {
+		log.Error().Err(err).Msgf("error sending back reva binary")
+		return
+	}
+}
+
+func binaryRevadName(os, arch, version string) string {
+	name := "revad_" + os + "_" + arch
+	if version != "" && version != "latest" {
+		name += "_" + version
+	}
+	return name
+}
+
+func parseDownloadRequest(r *http.Request) (*downloadRequest, error) {
+	var req downloadRequest
+	q := r.URL.Query()
+	req.OS = q.Get("os")
+	if req.OS == "" {
+		return nil, errors.New("os is required")
+	}
+	req.Arch = q.Get("arch")
+	if req.Arch == "" {
+		return nil, errors.New("arch is required")
+	}
+	req.RevaVersion = q.Get("version")
+	// this can be emtpy, in this case the builder
+	// will only build reva without plugins
+	req.Plugins = q["plugin"]
+	return &req, nil
+}
+
+func parsePlugins(plugins []string) ([]builder.Plugin, error) {
+	p := make([]builder.Plugin, 0, len(plugins))
+	for _, plugin := range plugins {
+		p = append(p, parsePlugin(plugin))
+	}
+	return p, nil
+}
+
+func parsePlugin(s string) builder.Plugin {
+	var p builder.Plugin
+	split := strings.SplitN(s, "@", 2)
+	p.RepositoryPath = split[0]
+	if len(split) > 1 {
+		p.Version = split[1]
+	}
+	return p
+}
+
+func (s *Builder) listPlugins(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+func (s *Builder) registerPlugin(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+func (s *Builder) Handler() http.Handler { return s.router }
+
+func (s *Builder) Prefix() string { return "gaia" }
+
+func (s *Builder) Close() error { return nil }
+
+func (s *Builder) Unprotected() []string { return []string{"/"} }
+
+var _ global.Service = (*Builder)(nil)
+var _ global.NewService = New
---
+++ service/go.mod
@@ -0,0 +1,25 @@
+module github.com/cs3org/gaia/service
+
+go 1.20
+
+require (
+	github.com/cs3org/gaia v0.0.0-20230721064608-781e18d99071
+	github.com/cs3org/reva v1.24.1-0.20230720105710-7c8eb90505b4
+	github.com/go-chi/chi/v5 v5.0.10
+)
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.14.1 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/rs/zerolog v1.29.1 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+)
